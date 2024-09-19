9d0ac639f6afe4ebfb1d3ceac738b482c1229d52
SohelRihan <sohelrihan09@gmail.com>
2024-09-19 20:36:36 +0600

    balance updated

--- js/home.js
+++ js/home.js
@@ -12,6 +12,28 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    // step-3: verify the pin number
+
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account');
+
+        // step-4: get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance);
+
+        // step-5: add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-6: update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('failed to add money! Please try again');
+    }
 });
 
 
