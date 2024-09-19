a73613842bdad72a9ec8fdb2bc0e142459ec53f5
SohelRihan <sohelrihan09@gmail.com>
2024-09-19 19:11:36 +0600

    capture input field

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
-console.log('home added');
+
+// add money to the account
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+
+    // prevent page reload after form service
+    event.preventDefault();
+
+    // step-2: get money to be added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get the pin number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+});
+
+
+
